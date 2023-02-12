71a0c9fbae5bd68c2187e006bf3b48d12895408c
ghav95 <112890783+ghav95@users.noreply.github.com>
2023-02-12 13:25:03 -0300

    Delete ICalculadoraDeFrete.java

--- src/service/ICalculadoraDeFrete.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import model.Produto;
-
-public interface ICalculadoraDeFrete extends ICalculadora<Produto>{
-    Double calcular(Produto produto, Double km) throws FreteIndisponivelException;
-}
